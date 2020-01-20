f6f46c366ea1bcfb58c0ffad1d869440b85eb240
Gabriel <gabrielborc@gmail.com>
2020-01-20 00:15:46 -0300

    Desenvolvido funcionalidade para aparecer devs que se cadastraram, conforme a pesquisa do usuário pelo websocket

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api  from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
@@ -21,6 +22,7 @@ function Main({ navigation }) {
                 });
 
                 const { latitude, longitude } = coords;
+                console.log(latitude, longitude);
 
                 setCurrentRegion({
                     latitude,
@@ -34,6 +36,24 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => {
+            setDevs([...devs, dev])
+        });
+    }, [devs]);
+
+    function setupWebsocket() {
+        disconnect();
+
+        const { latitude, longitude } = currentRegion;
+
+        connect(
+            longitude,
+            latitude,
+            techs
+        );
+    }
+
     async function loadDevs() {        
         const { latitude, longitude } = currentRegion;
         
@@ -46,6 +66,7 @@ function Main({ navigation }) {
         });
         
         setDevs(response.data.devs);
+        setupWebsocket();
     }
 
     function handleRegionChanged(region) {
---
+++ mobile/src/services/socket.js
@@ -0,0 +1,37 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.50.51:3333', {
+    autoConnect: false,
+});
+
+function subscribeToNewDevs(subscribeFunction) {
+   socket.on('new-dev', subscribeFunction) ;
+}
+
+function connect(latitude, 
+    
+    , techs) {
+    socket.io.opts.query = {
+        longitude,
+        latitude,
+        techs,
+    }
+
+    socket.connect();
+
+    socket.on('message', text => {
+        console.log(text);
+    })
+}
+
+function  disconnect() {
+    if (socket.connected) {
+        socket.disconnect();
+    }
+}
+
+export {
+    connect,
+    disconnect,
+    subscribeToNewDevs,
+}
